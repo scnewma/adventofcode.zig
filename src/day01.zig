ee08465169bc551c3e6cfaaa84954b1917b34e35
Shaun Newman <newman350z@gmail.com>
2023-12-29 09:52:40 -0700

    refactor to individual funcs

--- src/day01.zig
+++ src/day01.zig
@@ -1,4 +1,41 @@
 const std = @import("std");
+const testing = std.testing;
+
+fn readStdinString(allocator: std.mem.Allocator) ![]const u8 {
+    var stdin = std.io.getStdIn();
+    const stat = try stdin.stat();
+    return try stdin.reader().readAllAlloc(allocator, stat.size);
+}
+
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    const input = try readStdinString(allocator);
+
+    std.debug.print("Part 01: {}\n", .{try part01(allocator, input)});
+    std.debug.print("Part 02: {}\n", .{try part02(allocator, input)});
+}
+
+fn part01(allocator: std.mem.Allocator, input: []const u8) !usize {
+    var ans: usize = 0;
+    var lines = std.ArrayList([]const u8).init(allocator);
+    defer lines.deinit();
+    var linesIter = std.mem.tokenize(u8, input, "\n");
+    while (linesIter.next()) |line| {
+        var left: ?usize = null;
+        var right: usize = 0;
+
+        for (line) |ch| {
+            if (std.ascii.isDigit(ch)) {
+                left = left orelse ch - '0';
+                right = ch - '0';
+            }
+        }
+        ans += (left.? * 10) + right;
+    }
+    return ans;
+}
 
 const WORDS = [_][]const u8{
     "one",
@@ -12,29 +49,14 @@ const WORDS = [_][]const u8{
     "nine",
 };
 
-pub fn main() !void {
-    var stdin = std.io.getStdIn();
-    var bufr = std.io.bufferedReader(stdin.reader());
-    var reader = bufr.reader();
-
-    var p1sum: usize = 0;
-    var p2sum: usize = 0;
-    var buffer: [64]u8 = undefined;
-    while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+fn part02(allocator: std.mem.Allocator, input: []const u8) !usize {
+    var ans: usize = 0;
+    var lines = std.ArrayList([]const u8).init(allocator);
+    defer lines.deinit();
+    var linesIter = std.mem.tokenize(u8, input, "\n");
+    while (linesIter.next()) |line| {
         var left: ?usize = null;
         var right: usize = 0;
-
-        for (line) |ch| {
-            if (std.ascii.isDigit(ch)) {
-                left = left orelse ch - '0';
-                right = ch - '0';
-            }
-        }
-        p1sum += (left.? * 10) + right;
-
-        // part 2
-        left = null;
-        right = 0;
 
         for (line, 0..) |ch, i| {
             if (std.ascii.isDigit(ch)) {
@@ -49,9 +71,17 @@ pub fn main() !void {
                 }
             }
         }
-        p2sum += (left.? * 10) + right;
+        ans += (left.? * 10) + right;
     }
+    return ans;
+}
 
-    std.debug.print("Part 01: {}\n", .{p1sum});
-    std.debug.print("Part 02: {}\n", .{p2sum});
+const INPUT = @embedFile("inputs/day01.txt");
+
+test "part01" {
+    try testing.expect(try part01(testing.allocator, INPUT) == 54951);
+}
+
+test "part02" {
+    try testing.expect(try part02(testing.allocator, INPUT) == 55218);
 }
