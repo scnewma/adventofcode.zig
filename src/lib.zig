a51e22a44f8f34ba314be4c9fc8f9a0447bbb8d6
Shaun Newman <newman350z@gmail.com>
2024-01-01 13:01:23 -0700

    removes need for inline for in executeDay

--- src/lib.zig
+++ src/lib.zig
@@ -5,33 +5,52 @@ const Solution = struct {
     part02: []const u8,
 };
 
-const Day = struct {
-    no: usize,
-    import: type,
+const DAYS = [_]*const fn (std.mem.Allocator, []const u8) anyerror!Solution{
+    &dayExecutor(1),
+    &dayExecutor(2),
+    &dayExecutor(3),
+    &dayExecutor(4),
 };
 
-const DAYS = [_]Day{
-    .{ .no = 1, .import = @import("./day01.zig") },
-    .{ .no = 2, .import = @import("./day02.zig") },
-    .{ .no = 3, .import = @import("./day03.zig") },
-    .{ .no = 4, .import = @import("./day04.zig") },
-};
+fn dayExecutor(comptime no: comptime_int) fn (std.mem.Allocator, []const u8) anyerror!Solution {
+    const import = switch (no) {
+        1 => blk: {
+            break :blk @import("./day01.zig");
+        },
+        2 => blk: {
+            break :blk @import("./day02.zig");
+        },
+        3 => blk: {
+            break :blk @import("./day03.zig");
+        },
+        4 => blk: {
+            break :blk @import("./day04.zig");
+        },
+        else => @compileError("day not yet configured"),
+    };
 
-pub fn executeDay(allocator: std.mem.Allocator, input: []const u8, no: usize) !Solution {
-    // this has to be an inline for instead of an array lookup because the
-    // types differ at compile time.
-    inline for (DAYS) |day| {
-        if (day.no == no) {
-            const part01 = try day.import.part01(allocator, input);
-            const part02 = try day.import.part02(allocator, input);
+    const solver = struct {
+        fn solve(allocator: std.mem.Allocator, input: []const u8) !Solution {
+            const part01 = try import.part01(allocator, input);
+            const part02 = try import.part02(allocator, input);
             return .{
                 .part01 = try std.fmt.allocPrint(allocator, "{d}", .{part01}),
                 .part02 = try std.fmt.allocPrint(allocator, "{d}", .{part02}),
             };
         }
-    }
+    };
 
-    return error.NotYetSolved;
+    return solver.solve;
+}
+
+pub fn executeDay(allocator: std.mem.Allocator, input: []const u8, no: usize) !Solution {
+    if (no < 1 or no > 25) {
+        return error.InvalidDay;
+    }
+    if (no > DAYS.len) {
+        return error.NotYetSolved;
+    }
+    return DAYS[no - 1](allocator, input);
 }
 
 test {
